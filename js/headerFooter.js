afcebbbdc36043dc131d02a69a92320e2c44122b
hugo <hugo0807pereira@gmail.com>
2022-11-22 18:58:52 +0100

    page itinéraire

---
+++ js/headerFooter.js
@@ -0,0 +1,20 @@
+document.querySelector('header').innerHTML += `
+    <div class="container">
+        <img src="../img/sncf_logo.png" alt="logo SNCF">
+        <h1>Mon espace</h1>
+    </div>
+`
+
+document.querySelector('footer').innerHTML += `
+    <div class="container">
+        <a href="https://tgvinoui.sncf/mentions-legales" target="_blank"><p>Mention légales</p></a>
+        <hr>
+        <a href="https://tgvinoui.sncf/conditions-generales" target="_blank"><p>Conditions générales</p></a>
+        <hr>
+        <a href="https://tgvinoui.sncf/cookies" target="_blank"><p>Cookies</p></a>
+        <hr>
+        <a href="https://tgvinoui.sncf/plan-du-site" target="_blank"><p>Plan du site</p></a>
+        <hr>
+        <a href="https://tgvinoui.sncf/accessibilite" target="_blank"><p>Accéssibilité</p></a>
+    </div>
+`
